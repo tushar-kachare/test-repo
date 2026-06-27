2d824ceb1efc56d3995843388e75523b650bae28
tushar-kachare <kacharetushar05@gmail.com>
2026-06-27 18:06:48 +0530

    test: trigger on railway deployment

--- somefile.js
+++ somefile.js
@@ -2,5 +2,7 @@
 const app = () => {
     // this is just to check comments added by reviewPilot
     console.log("just checking if any comments gets added for correct function");
+
+    console.log("this comment is after adding railway deploy this doesn't has closing bracket";
 }
 app();
