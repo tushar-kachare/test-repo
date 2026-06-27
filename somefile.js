de05e8449d84bc88ab3d3ffd76bb7a0a7aa03410
tushar-kachare <kacharetushar05@gmail.com>
2026-06-27 16:56:14 +0530

    test: updated function to make it correct

--- somefile.js
+++ somefile.js
@@ -1,4 +1,6 @@
 // test change for reviewpilot
-const app = () {
+const app = () => {
     // this is just to check comments added by reviewPilot
+    console.log("just checking if any comments gets added for correct function");
 }
+app();
