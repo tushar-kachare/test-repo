38bf8fe2187201fe71ec38e18777baba6f567714
tushar-kachare <kacharetushar05@gmail.com>
2026-06-27 18:34:43 +0530

    test: final test after fixing bugs

--- somefile.js
+++ somefile.js
@@ -3,6 +3,9 @@ const app = () => {
     // this is just to check comments added by reviewPilot
     console.log("just checking if any comments gets added for correct function");
 
-    console.log("this comment is after adding railway deploy this doesn't has closing bracket";
+    console.log("this comment is after adding railway deploy this doesn't has closing bracket");
+
+    console.log("testing final fix")
+    i should not write this statement
 }
 app();
